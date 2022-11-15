23795c9615ad2308f3905e36270e05445e2bfcce
Reem Ahmed <reem.mda0909@gmail.com>
2022-11-15 12:16:33 -0700

    get all post

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const postRoutes = require('./post-routes');
 
 router.use('/api', apiRoutes);
+router.use('/posts', postRoutes);
 
 router.use((req, res) => {
   res.status(404).end();

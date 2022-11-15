94817e52fdcf2d6607cc68451365c916c42c7b93
Reem Ahmed <reem.mda0909@gmail.com>
2022-11-15 14:34:35 -0700

    create vote models

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,12 +1,26 @@
+const sequelize = require('../../config/connection');
+
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
     Post.findAll({
-      attributes: ['id', 'post_url', 'title', 'created_at'],
+      attributes: [
+        'id', 'post_url', 'title', 'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
       order: [['created_at', 'DESC']], 
       include: [
+          // include the Comment model here:
+   {
+    model: Comment,
+    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+    include: {
+      model: User,
+      attributes: ['username']
+    }
+  },
         {
           model: User,
           attributes: ['username']
@@ -26,8 +40,19 @@ router.get('/', (req, res) => {
       where: {
         id: req.params.id
       },
-      attributes: ['id', 'post_url', 'title', 'created_at'],
+      attributes: ['id', 'post_url', 'title', 'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
       include: [
+          // include the Comment model here:
+   {
+    model: Comment,
+    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+    include: {
+      model: User,
+      attributes: ['username']
+    }
+  },
         {
           model: User,
           attributes: ['username']
@@ -63,6 +88,16 @@ router.get('/', (req, res) => {
       });
   });
 
+// PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+    // custom static method created in models/Post.js
+    Post.upvote(req.body, { Vote })
+      .then(updatedPostData => res.json(updatedPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  });
   // update post
 
   router.put('/:id', (req, res) => {
@@ -88,5 +123,26 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
       });
   });
+
+  router.delete('/:id', (req, res) => {
+    Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  
 
   module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const postRoutes = require('./post-routes');
 
 router.use('/api', apiRoutes);
-router.use('/posts', postRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
